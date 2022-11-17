d4a1d6466dd8725edc7e5e3b314d8a191d9db052
Filippo Albertini <filippo@filippoalbertini.it>
2022-11-17 12:54:56 +0100

    Gestione figure partendo dalla dichiarazione sulla classe astratta e istanziazione su quella specifica

--- AS2223_4G_INF_Prof_Figure/Program.cs
+++ AS2223_4G_INF_Prof_Figure/Program.cs
@@ -2,13 +2,16 @@
 
 using AS2223_4G_INF_Prof_Figure;
 
-Console.WriteLine("Prof;AS 22/23 4G;17/11/22; Gestione figure geometriche");
+Console.WriteLine("Prof;AS 22/23 4G;17/11/22; Gestione figure geometriche - branch 2");
 
-Quadrato q = new Quadrato(4);
-Rettangolo r = new Rettangolo(4,2);
-Cerchio c = new Cerchio(4);
+Figura f = new Figura();
 
-Console.WriteLine($"Il quadrato ha area {q.Area()} e perimetro {q.Perimetro()}");
-Console.WriteLine($"Il rettangolo ha area {r.Area()} e perimetro {r.Perimetro()}");
-Console.WriteLine($"Il cerchio ha area {c.Area()} e perimetro {c.Perimetro()}");
+f = new Quadrato(4);
+Console.WriteLine($"Il {f.Nome} ha area {f.Area()} e perimetro {f.Perimetro()}");
+
+f = new Rettangolo(4,2);
+Console.WriteLine($"Il {f.Nome} ha area {f.Area()} e perimetro {f.Perimetro()}");
+
+f = new Cerchio(4);
+Console.WriteLine($"Il {f.Nome} ha area {f.Area()} e perimetro {f.Perimetro()}");
 
