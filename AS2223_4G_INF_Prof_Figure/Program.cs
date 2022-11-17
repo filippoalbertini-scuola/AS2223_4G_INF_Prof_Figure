2d6f1012ab0ce4ad41303229290de49cdc50db37
Filippo Albertini <filippo@filippoalbertini.it>
2022-11-17 11:26:16 +0100

    Sistemazione main

--- AS2223_4G_INF_Prof_Figure/Program.cs
+++ AS2223_4G_INF_Prof_Figure/Program.cs
@@ -2,20 +2,13 @@
 
 using AS2223_4G_INF_Prof_Figure;
 
+Console.WriteLine("Prof;AS 22/23 4G;17/11/22; Gestione figure geometriche");
+
 Quadrato q = new Quadrato(4);
 Rettangolo r = new Rettangolo(4,2);
 Cerchio c = new Cerchio(4);
 
-double area;
-    
-area = q.Area();
-area = r.Area();
-area = c.Area();
+Console.WriteLine($"Il quadrato ha area {q.Area()} e perimetro {q.Perimetro()}");
+Console.WriteLine($"Il rettangolo ha area {r.Area()} e perimetro {r.Perimetro()}");
+Console.WriteLine($"Il cerchio ha area {c.Area()} e perimetro {c.Perimetro()}");
 
-double perimetro;
-
-perimetro = q.Perimetro();
-perimetro = r.Perimetro();
-perimetro = c.Perimetro();
-
-Console.WriteLine("Hello, World!");
